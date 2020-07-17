6c32ac4435d0e413cc7cd54b544db14d9a252222
L. F <l.f.stack.exchange@gmail.com>
2020-07-17 12:58:58 +0800

    Add cooldown system

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@
 
 //! A simple battle game.
 
+pub mod cooldown;
 pub mod game;
 pub mod game_controller;
 pub mod game_view;
 pub mod object;
 pub mod player;
 
+pub use cooldown::Cooldown;
 pub use game::{Cell, Game, GameBuilder, GameSettings};
 pub use game_controller::{GameController, GameControllerSettings, KeyBinding};
 pub use game_view::{GameView, GameViewSettings};
